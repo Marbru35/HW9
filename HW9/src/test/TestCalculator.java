27b1ac00803dced42a05a9fd4ffefa28f815fcc5
Marlon <mspiess1@smail.uni-koeln.de>
2022-12-07 09:39:24 +0100

    initial commmit + exercise 1

---
+++ HW9/src/test/TestCalculator.java
@@ -0,0 +1,54 @@
+package test;
+
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import main.Calculator;
+
+
+class TestCalculator {
+
+	Calculator calculator = new Calculator();
+
+	@Test
+	public void Should_add_two_numbers_and_return_result() {
+		int expectedResult = 2 + 3;
+		int actualResult = calculator.add(2, 3);
+		
+		assertEquals(expectedResult, actualResult);
+	}
+	
+	@Test
+	public void Should_substract_two_numbers_and_return_result() {
+		int expectedResult = 5 - 10;
+		int actualResult = calculator.sub(5, 10);
+		
+		assertEquals(expectedResult, actualResult);
+	}
+	
+	@Test
+	public void Should_multiply_two_numbers_and_return_result() {
+		int expectedResult = 2 * 3;
+		int actualResult = calculator.multiply(2, 3);
+		
+		assertEquals(expectedResult, actualResult);
+	}
+	
+	@Test
+	public void Should_divide_two_numbers_and_return_result() {
+		float expectedResult = 10f / 3;
+		float actualResult = calculator.divide(10, 3);
+		
+		assertEquals(expectedResult, actualResult);
+	}
+	
+	@Test
+	public void Should_throw_an_arithmetic_exception_if_denominator_is_zero() {
+		assertThrows(ArithmeticException.class, () -> calculator.divide(10,0));
+	}
+
+}
